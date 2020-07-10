813e89e5231365ea9c23d030ea2dd58da2d8711f
Shivangi Gupta <shivangi.gupta0796@gmail.com>
2020-07-10 21:45:22 +0530

    negative numbers add test

--- src/com/stringCalculator/StringCalculator.java
+++ src/com/stringCalculator/StringCalculator.java
@@ -1,11 +1,11 @@
 package com.stringCalculator;
 
 public class StringCalculator {
-	public int Add(String numbers) {
+	public int Add(String numbers) throws Exception {
 		int result=0;
 		String[] arr = null;
 		String delimiter = "[,\n]+";
-		
+		String negativeNumber="";
 		if(numbers.length()==1) {
 			return Integer.parseInt(numbers);
 		}
@@ -16,13 +16,24 @@ public class StringCalculator {
 			String newNumber = numbers.substring(endIndex+1);
 			arr = newNumber.split("["+delimiter+"]+");
 			for(int i =0;i<arr.length;i++) {
-				result = result + Integer.parseInt(arr[i]);
+				if(Integer.parseInt(arr[i])<0) {
+					negativeNumber = negativeNumber+arr[i]+",";
+				}
+				if(negativeNumber.length()==0) {
+					result = result + Integer.parseInt(arr[i]);
+				}
+			}
+			if(negativeNumber.length()>0) {
+				throw new Exception("negatives not allowed:"+negativeNumber.substring(0, negativeNumber.length()-1));
 			}
 			return result;
 		}
 		else if(numbers.length()>1 && numbers.contains(",")) {
 			arr = numbers.split(delimiter);
 			for(int i =0;i<arr.length;i++) {
+				if(Integer.parseInt(arr[i])<0) {
+					throw new Exception("negatives not allowed");
+				}
 				result = result + Integer.parseInt(arr[i]);
 			}
 			return result;
--- src/com/stringCalculator/StringCalculatorTest.java
+++ src/com/stringCalculator/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.stringCalculator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -9,43 +10,85 @@ public class StringCalculatorTest {
 	@Test
 	public void noNumberAddTest() {
 		StringCalculator sc = new StringCalculator();
-		int result = sc.Add("");
+		int result = 0;
+		try {
+			result = sc.Add("");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		assertEquals(0,result);
 	}
 	
 	@Test
 	public void oneNumberAddTest() {
 		StringCalculator sc = new StringCalculator();
-		int result = sc.Add("1");
+		int result = 0;
+		try {
+			result = sc.Add("1");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		assertEquals(1,result);
 	}
 	
 	@Test
 	public void twoNumberAddTest() {
 		StringCalculator sc = new StringCalculator();
-		int result = sc.Add("1,2");
+		int result = 0;
+		try {
+			result = sc.Add("1,2");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		assertEquals(3,result);
 	}
 	
 	@Test
 	public void unknownNumbersAddTest() {
 		StringCalculator sc = new StringCalculator();
-		int result = sc.Add("1,2,3,4,5");
+		int result = 0;
+		try {
+			result = sc.Add("1,2,3,4,5");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		assertEquals(15,result);
 	}
 	
 	@Test
 	public void newLineAddTest() {
 		StringCalculator sc = new StringCalculator();
-		int result = sc.Add("1\n2,3");
+		int result = 0;
+		try {
+			result = sc.Add("1\n2,3");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		assertEquals(6,result);
 	}
 	
 	@Test
 	public void differentDelimeterAddTest() {
 		StringCalculator sc = new StringCalculator();
-		int result = sc.Add("//:,;\n1;2,3:5,1");
+		int result = 0;
+		try {
+			result = sc.Add("//:,;\n1;2,3:5,1");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		assertEquals(12,result);
+	}
+	
+	@Test
+	public void negativeNumberTest() {
+		StringCalculator sc = new StringCalculator();
+		try {
+			sc.Add("//:,;\n1;2,-3:-5,-1");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+			assertTrue(e.getMessage().contains("negatives not allowed"));
+		}
 	}
 	
 }
